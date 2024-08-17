560eb48a34ce5c94f09c3c8fb896d7225771d678
sdfg <dfs@dfgsadf.tr>
2024-08-17 06:53:15 +0200

    heirlooms - labeling of items #4

--- trigger_all_heirlooms_auto.sql
+++ trigger_all_heirlooms_auto.sql
@@ -63,32 +63,32 @@ replace into `battlenet_account_heirlooms` (`accountId`, `itemId`, `flags`) VALU
 (NEW.`id`, '122377', '63'), -- Ausdauernde Schiftung des ungezähmten Herzens - Leder Schulter
 (NEW.`id`, '122378', '63'), -- Exquisiter Mantel des blinden Sehers - Stoff Schulter
 (NEW.`id`, '122379', '63'), -- Todesbotenbrustplatte des Champions - Kette Brust
-(NEW.`id`, '122380', '63'), --
-(NEW.`id`, '122381', '63'), --
-(NEW.`id`, '122382', '63'), --
-(NEW.`id`, '122383', '63'), --
-(NEW.`id`, '122384', '63'), --
-(NEW.`id`, '122385', '63'), --
-(NEW.`id`, '122386', '63'), --
-(NEW.`id`, '122387', '63'), --
-(NEW.`id`, '122388', '63'), --
-(NEW.`id`, '122389', '63'), --
-(NEW.`id`, '122390', '63'), --
-(NEW.`id`, '122391', '63'), --
-(NEW.`id`, '122392', '63'), --
-(NEW.`id`, '122396', '63'), --
-(NEW.`id`, '122529', '63'), --
-(NEW.`id`, '122530', '63'), --
-(NEW.`id`, '122662', '63'), --
-(NEW.`id`, '122663', '63'), --
-(NEW.`id`, '122664', '63'), --
-(NEW.`id`, '122666', '63'), --
-(NEW.`id`, '122667', '63'), --
-(NEW.`id`, '122668', '63'), --
-(NEW.`id`, '126948', '15'), --
-(NEW.`id`, '126949', '15'), --
-(NEW.`id`, '127010', '63'), --
-(NEW.`id`, '127011', '63'), --
+(NEW.`id`, '122380', '63'), -- Mystische Weste der Elemente - Kette Brust
+(NEW.`id`, '122381', '63'), -- Polierte Brustplatte der Ehre - Platte Brust
+(NEW.`id`, '122382', '63'), -- Geputzte Eisenfederbrustplatte - Leder Brust
+(NEW.`id`, '122383', '63'), -- Befleckte Tunika der Schattenkunst - Leder Brust
+(NEW.`id`, '122384', '63'), -- Zerlumpte Robe der Furcht - Stoff Brust
+(NEW.`id`, '122385', '63'), -- Ehrwürdige Masse von McGowan - 1H Streitkolben
+(NEW.`id`, '122386', '63'), -- Wiederverwendeter Lavagreifer - 2H Streitkolben
+(NEW.`id`, '122387', '63'), -- Brünierte Brustplatte der Macht - Platte Brust
+(NEW.`id`, '122388', '63'), -- Brünierte Schulterstücke der Macht - Platte Schulter
+(NEW.`id`, '122389', '63'), -- Blutgetränkter Häscher der Schädelesse - 1H Schwert
+(NEW.`id`, '122390', '63'), -- Staubiger Foliant der Verlorenen - Schildhand
+(NEW.`id`, '122391', '63'), -- Versengter drakonischer Deflektor - Schild
+(NEW.`id`, '122392', '63'), -- Verwitterter Schild des Beobachters - Schild
+(NEW.`id`, '122396', '63'), -- Messerkrallen des Grubenkämpfers - 1H Faustwaffe
+(NEW.`id`, '122529', '63'), -- Schreckenspiratenring - Ring
+(NEW.`id`, '122530', '63'), -- Geerbtes Mal der Tyrannei - Schmuck
+(NEW.`id`, '122662', '63'), -- Ewiger Talisman des Entrinnens - Hals
+(NEW.`id`, '122663', '63'), -- Ewiges Amulett des Erlösten - Hals
+(NEW.`id`, '122664', '63'), -- Ewiger Halsreif des Horizonts - Hals
+(NEW.`id`, '122666', '63'), -- Ewige gewobene Efeuhalskette - Hals
+(NEW.`id`, '122667', '63'), -- Ewiger Talisman des glühenden Zorn - Hals
+(NEW.`id`, '122668', '63'), -- Ewiger Wille des Märtyrers - Hals
+(NEW.`id`, '126948', '15'), -- Verteidigender Champion - Schmuck
+(NEW.`id`, '126949', '15'), -- Zurückkehrender Champion - Schmuck
+(NEW.`id`, '127010', '63'), -- Makellose Brustplatte der Lichtschmiede - Platte Brust
+(NEW.`id`, '127011', '63'), -- Makellose Beinplatten der Lichtschmiede - Platte Beine
 (NEW.`id`, '127012', '63'), --
 (NEW.`id`, '128169', '63'), --
 (NEW.`id`, '128172', '63'), --
